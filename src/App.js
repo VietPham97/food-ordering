6d7ab6583f7c5a83630176e4ae9c7f778b7c76ad
VietPhamCa <vietpham1097@gmail.com>
2022-12-04 00:05:42 -0500

    Add a Cart modal component through a React Portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  
+  const showCart = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCart = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCart} />}
+      <Header onShowCart={showCart} />
       <main>
         <Meals />
       </main>
